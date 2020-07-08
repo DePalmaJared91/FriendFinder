602e9df65b3bf3c16602ef2bed1949983e6ae7ba
Jared DePalma <depalmajared91@gmail.com>
2020-07-07 19:32:33 -0600

    Got the people and image links in but I need to finsih the connection and some routes are not working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,75 +1,52 @@
 var friends = [
     {
-        name: "",
+        name: "Kim Jung Un",
 
-        photo: "",
+        photo: "https://ichef.bbci.co.uk/news/320/cpsprodpb/FAEC/production/_105863246_052682295.jpg",
 
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-
-    },
-
-    {
-
-        name: "",
-
-        photo: "",
-
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-
-
-
+        scores: [5, 2, 5, 2, 2, 2, 4, 3, 2, 3]
     },
     {
-        name: "",
+        name: "Xi Jinping",
 
-        photo: "",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Xi_Jinping_2019.jpg/220px-Xi_Jinping_2019.jpg",
 
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-
-
-
-
-
+        scores: [4, 2, 5, 1, 2, 4, 2, 5, 4, 2]
     },
     {
-        name: "",
+        name: "Vladamir Putin",
 
-        photo: "",
+        photo: "https://cdn.cnn.com/cnnnext/dam/assets/200618182033-putin-0618-large-169.jpg",
 
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-
-
-
-
+        scores: [5, 4, 5, 5, 5, 3, 4, 5, 4, 3]
     },
     {
-        name: "",
+        name: "Arnold Schwarzenegger",
 
-        photo: "",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Arnold_Schwarzenegger_1974.jpg/220px-Arnold_Schwarzenegger_1974.jpg",
 
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-
-
-
-
+        scores: [5, 5, 3, 3, 4, 4, 4, 5, 5, 5]
     },
     {
-        name: "",
+        name: "Paris Hilton",
 
-        photo: "",
+        photo: "https://i.dailymail.co.uk/1s/2020/05/02/17/27925940-8280705-Fan_love_Paris_Hilton_above_February_2020_loves_her_fans_so_much-m-20_1588435975335.jpg",
 
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-
-
-
-
+        scores: [5, 3, 5, 5, 1, 4, 5, 2, 5, 5]
     },
     {
-        name: "",
+        name: "Mark Zuckerberg",
 
-        photo: "",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/1/18/Mark_Zuckerberg_F8_2019_Keynote_%2832830578717%29_%28cropped%29.jpg",
 
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        scores: [3, 3, 5, 2, 2, 2, 2, 5, 5, 1]
+    },
+    {
+        name: "Genghis Khan",
+
+        photo: "https://cdn.britannica.com/73/121673-050-4FE5679B/Genghis-Khan-ink-colour-silk-Taipei-Taiwan.jpg",
+
+        scores: [5, 3, 3, 3, 3, 3, 3, 4, 4, 4]
     }
 ];
 module.exports = friends;
